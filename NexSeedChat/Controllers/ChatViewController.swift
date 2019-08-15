ff3bf3064299c24efdc9a670e6e5834da5e08d58
堀川浩二 <>
2019-08-15 09:27:26 +0800

    初期設定

---
+++ NexSeedChat/Controllers/ChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewController.swift
+//  NexSeedChat
+//
+//  Created by 堀川浩二 on 2019/08/15.
+//  Copyright © 2019 堀川浩二. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NexSeedChat/Objects/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  NexSeedChat
+//
+//  Created by 堀川浩二 on 2019/08/15.
+//  Copyright © 2019 堀川浩二. All rights reserved.
+//
+
+import Foundation
